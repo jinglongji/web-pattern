d7b585ecf0fea0379fe5b364b73d385bfd76a6f5
jinglongji <jinglongji@outlook.com>
2022-02-09 12:21:41 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <img alt="Compass logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello World" />
+</template>
+
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue';
+</script>
